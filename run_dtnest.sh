e540c756fa35808b04f4cdf99922993af399ab82
Jeff Smits <j.smits-1@tudelft.nl>
2026-03-23 14:31:23 +0100

    Fix arguments to run_dtnest.sh

--- run_dtnest.sh
+++ run_dtnest.sh
@@ -2,13 +2,12 @@
 
 # argument 1: path/to/model-file
 # argument 2: path/to/props-file
-# argument 3: add-dont-care-action ("" or "--add-dont-care-action")
-# argument 4: subtree_depth (integer, default 7)
-# argument 5: timeout (integer, seconds, default 1200)
+# argument 3: path/to/output-directory
+# argument 4: add-dont-care-action ("" or "--add-dont-care-action")
 
 MODELNAME=$(basename -- "$1")
 PROPNAME=$(basename -- "$2")
 COMMAND=python3
-ARGS="/opt/paynt/paynt.py /opt/paynt/models/dts-uai/ --sketch $MODELNAME --props $PROPNAME $3 --subtree-depth $4 --timeout $5 --export-synthesis /opt/paynt/models/results/tree"
+ARGS="/opt/paynt/paynt.py /opt/paynt/models/dts-uai/ --sketch $MODELNAME --props $PROPNAME $4"
 
-docker run --volume="$1:/opt/paynt/models/dts-uai/$MODELNAME:ro" --volume="$2:/opt/paynt/models/dts-uai/$PROPNAME:ro" dtpaynt "$COMMAND" $ARGS
+docker run --volume="$1:/opt/paynt/models/dts-uai/$MODELNAME:ro" --volume="$2:/opt/paynt/models/dts-uai/$PROPNAME:ro" --volume="$3:/opt/paynt/models/dts-uai/decision_trees/default/scheduler" dtpaynt "$COMMAND" $ARGS
