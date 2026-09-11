0cedecbb2113689a84ff137b9f0fdde4efaae9cd
Daniel Vos <daniel.vos.d@gmail.com>
2026-09-11 14:25:44 +0200

    Fix dtnest by running dtcontrol first

--- run_dtnest.sh
+++ run_dtnest.sh
@@ -7,11 +7,14 @@
 # argument 5: path/to/output-directory
 # argument 6: add-dont-care-action ("" or "--add-dont-care-action")
 
+# TODO: do not polute models/dts-uai/ but create a temp dir
+
 MODELNAME=$(basename -- "$1")
 PROPNAME=$(basename -- "$2")
 # Command to execute dtNESt
-DTNEST="python3 /opt/paynt/paynt.py /opt/paynt/models/dts-uai/ --sketch $MODELNAME --props $PROPNAME $6"
+DTCONTROL="dtcontrol --input /opt/paynt/models/dts-uai/scheduler.storm.json --output /opt/paynt/models/dts-uai/decision_trees/ -r --use-preset default"
+DTNEST="python3 /opt/paynt/paynt.py /opt/paynt/models/dts-uai/ --sketch $MODELNAME --props $PROPNAME $6 --add-dont-care-action"
 # Editing the decision tree file directly as there is no other way to get the code to actually output the learned tree...
 SET_OUTPUT_NAME="sed -i '35i\\"$'\n'"        self.export_synthesis_filename_base = \"/opt/paynt/models/dts-uai/results/tree\""$'\n'"' /opt/paynt/paynt/synthesizer/decision_tree.py"
 
-docker run --volume="$1:/opt/paynt/models/dts-uai/$MODELNAME:ro" --volume="$2:/opt/paynt/models/dts-uai/$PROPNAME:ro" --volume="$3:/opt/paynt/models/dts-uai/state-valuations.json:ro" --volume="$4:/opt/paynt/models/dts-uai/decision_trees/default/scheduler/default.json:ro" --volume="$5:/opt/paynt/models/dts-uai/results" paynt:uai sh -c "$SET_OUTPUT_NAME; $DTNEST"
+docker run --volume="$1:/opt/paynt/models/dts-uai/$MODELNAME:ro" --volume="$2:/opt/paynt/models/dts-uai/$PROPNAME:ro" --volume="$3:/opt/paynt/models/dts-uai/state-valuations.json:ro" --volume="$4:/opt/paynt/models/dts-uai/scheduler.storm.json:ro" --volume="$5:/opt/paynt/models/dts-uai/results" paynt:uai sh -c "$SET_OUTPUT_NAME && $DTCONTROL && $DTNEST"
