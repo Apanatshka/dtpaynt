fdd75ad22573564cca15271f6f86c02571c8f518
Daniel Vos <daniel.vos.d@gmail.com>
2026-09-14 12:17:12 +0200

    Add timeout and container name

--- run_dtnest.sh
+++ run_dtnest.sh
@@ -5,6 +5,8 @@
 # argument 3: path/to/state-valuations-file
 # argument 4: path/to/default-scheduler-file
 # argument 5: path/to/output-directory
+# argument 6: timeout
+# argument 7: name for the docker container
 
 # TODO: do not polute models/dts-uai/ but create a temp dir
 
@@ -12,8 +14,9 @@ MODELNAME=$(basename -- "$1")
 PROPNAME=$(basename -- "$2")
 # Command to execute dtNESt
 DTCONTROL="dtcontrol --input /opt/paynt/models/dts-uai/scheduler.storm.json --output /opt/paynt/models/dts-uai/decision_trees/ -r --use-preset default"
-DTNEST="python3 /opt/paynt/paynt.py /opt/paynt/models/dts-uai/ --sketch $MODELNAME --props $PROPNAME --add-dont-care-action"
+# NOTE: DTNEST does not actually use the timout value
+DTNEST="python3 /opt/paynt/paynt.py /opt/paynt/models/dts-uai/ --sketch $MODELNAME --props $PROPNAME  --timeout $6 --add-dont-care-action"
 # Editing the decision tree file directly as there is no other way to get the code to actually output the learned tree...
 SET_OUTPUT_NAME="sed -i '35i\\"$'\n'"        self.export_synthesis_filename_base = \"/opt/paynt/models/dts-uai/results/tree\""$'\n'"' /opt/paynt/paynt/synthesizer/decision_tree.py"
 
-docker run --rm --volume="$1:/opt/paynt/models/dts-uai/$MODELNAME:ro" --volume="$2:/opt/paynt/models/dts-uai/$PROPNAME:ro" --volume="$3:/opt/paynt/models/dts-uai/state-valuations.json:ro" --volume="$4:/opt/paynt/models/dts-uai/scheduler.storm.json:ro" --volume="$5:/opt/paynt/models/dts-uai/results" paynt:uai sh -c "$SET_OUTPUT_NAME && $DTCONTROL && $DTNEST"
+docker run --name $7 --rm --volume="$1:/opt/paynt/models/dts-uai/$MODELNAME:ro" --volume="$2:/opt/paynt/models/dts-uai/$PROPNAME:ro" --volume="$3:/opt/paynt/models/dts-uai/state-valuations.json:ro" --volume="$4:/opt/paynt/models/dts-uai/scheduler.storm.json:ro" --volume="$5:/opt/paynt/models/dts-uai/results" paynt:uai sh -c "$SET_OUTPUT_NAME && $DTCONTROL && $DTNEST"
