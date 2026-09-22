c3952969ebd6c62cb6a3b1133ab675e8ff62cfb7
Daniel Vos <daniel.vos.d@gmail.com>
2026-09-22 10:28:20 +0200

    Add container names to dtpaynt

--- run_dtpaynt.sh
+++ run_dtpaynt.sh
@@ -6,10 +6,11 @@
 # argument 4: path/to/output-directory
 # argument 5: tree depth
 # argument 6: timeout
+# argument 7: name for the docker container
 
 MODELNAME=$(basename -- "$1")
 PROPNAME=$(basename -- "$2")
 COMMAND=python3
 ARGS="/opt/paynt/paynt.py /opt/cav25-experiments/benchmarks/ --sketch $MODELNAME --props $PROPNAME --tree-enumeration --tree-depth=$5 --timeout $6 --export-synthesis /opt/cav25-experiments/results/tree"
 
-docker run --rm --volume="$1:/opt/cav25-experiments/benchmarks/$MODELNAME:ro" --volume="$2:/opt/cav25-experiments/benchmarks/$PROPNAME:ro" --volume="$3:/opt/cav25-experiments/benchmarks/state-valuations.json:ro" --volume="$4:/opt/cav25-experiments/results" dtpaynt "$COMMAND" $ARGS
+docker run --name $7 --rm --volume="$1:/opt/cav25-experiments/benchmarks/$MODELNAME:ro" --volume="$2:/opt/cav25-experiments/benchmarks/$PROPNAME:ro" --volume="$3:/opt/cav25-experiments/benchmarks/state-valuations.json:ro" --volume="$4:/opt/cav25-experiments/results" dtpaynt "$COMMAND" $ARGS
